125cb1d6127df58a2c580f0b9e0359f9aeab4e90
Lucas Russo <lerwys@gmail.com>
2015-10-31 12:14:58 -0200

    get-autotools.sh: install pkg-config first as others depend on it

--- get-autotools.sh
+++ get-autotools.sh
@@ -10,22 +10,24 @@ echo "Installing Autotools"
 # Install compatible autotools version not available in some
 # distributions
 
+PKG_CONFIG_VERSION=0.26
 M4_VERSION=1.4.17
 AUTOCONF_VERSION=2.69
 AUTOMAKE_VERSION=1.14.1
 LIBTOOL_VERSION=2.4.2
-PKG_CONFIG_VERSION=0.26
 
+wget http://pkgconfig.freedesktop.org/releases/pkg-config-${PKG_CONFIG_VERSION}.tar.gz
 wget http://ftp.gnu.org/gnu/m4/m4-${M4_VERSION}.tar.gz
 wget http://ftp.gnu.org/gnu/autoconf/autoconf-${AUTOCONF_VERSION}.tar.gz
 wget http://ftp.gnu.org/gnu/automake/automake-${AUTOMAKE_VERSION}.tar.gz
 wget http://ftp.gnu.org/gnu/libtool/libtool-${LIBTOOL_VERSION}.tar.gz
-wget http://pkgconfig.freedesktop.org/releases/pkg-config-${PKG_CONFIG_VERSION}.tar.gz
 
 # Configure and Install libraries
-for project in m4-${M4_VERSION} autoconf-${AUTOCONF_VERSION} \
-        automake-${AUTOMAKE_VERSION} libtool-${LIBTOOL_VERSION} \
-        pkg-config-${PKG_CONFIG_VERSION}; do
+for project in pkg-config-${PKG_CONFIG_VERSION} \
+        m4-${M4_VERSION} \
+        autoconf-${AUTOCONF_VERSION} \
+        automake-${AUTOMAKE_VERSION} \
+        libtool-${LIBTOOL_VERSION}; do
     tar xzvf ${project}.tar.gz && \
         cd $project && \
         ./configure &&
