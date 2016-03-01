250e0b4e6247f85a756f9ee1db4199a2dad479ae
Lucas Russo <lerwys@gmail.com>
2016-03-01 10:23:21 -0300

    env-vars.sh: simplified PKG_CONFIG paths
    We just enumerate some common pkg_config
    paths

--- env-vars.sh
+++ env-vars.sh
@@ -7,10 +7,14 @@ set -e
 
 # Add GLIB pkg-config file (*.pc) to pkg-config path, so
 # a possible update on pkg-config works.
-GLIB_PKG_CONFIG_PATH=$(find /usr/ -type f -iname "glib-2.0.pc" -print | head -n 1)
+GLIB_PKG_CONFIG_PATH=/usr/lib64/pkgconfig
+SHARE_PKG_CONFIG_PATH=/usr/share/pkgconfig
+MORE_PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 
 # Don't care if PKG_CONFIG_PATH is unbounded here, as we might not have
 # this previously set
 set +u
-export PKG_CONFIG_PATH=${GLIB_PKG_CONFIG_PATH}${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${GLIB_PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${SHARE_PKG_CONFIG_PATH}
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:${MORE_PKG_CONFIG_PATH}
 set -u
