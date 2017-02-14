081f8268bd9e481ceff62e48387d567e42b7d537
Lucas Russo <lerwys@gmail.com>
2017-02-14 14:00:31 -0200

    get-epics.sh: append lines to epics.conf instead of overwriting

--- get-epics.sh
+++ get-epics.sh
@@ -64,10 +64,10 @@ ln -sf base-${EPICS_BASE_VERSION} base
 
 # Update ldconfig with EPICS libs
 sudo touch ${LDCONF_DIR}/epics.conf
-echo "${EPICS_BASE}/lib/${EPICS_HOST_ARCH}" | sudo tee /etc/ld.so.conf.d/epics.conf
-echo "/usr/lib64" | sudo tee /etc/ld.so.conf.d/epics.conf
-echo "/lib64" | sudo tee /etc/ld.so.conf.d/epics.conf
-echo "/usr/lib" | sudo tee /etc/ld.so.conf.d/epics.conf
+echo "${EPICS_BASE}/lib/${EPICS_HOST_ARCH}" | sudo tee -a /etc/ld.so.conf.d/epics.conf
+echo "/usr/lib64" | sudo -a tee /etc/ld.so.conf.d/epics.conf
+echo "/lib64" | sudo tee -a /etc/ld.so.conf.d/epics.conf
+echo "/usr/lib" | sudo tee -a /etc/ld.so.conf.d/epics.conf
 
 # Update ldconfig cache
 sudo ldconfig
