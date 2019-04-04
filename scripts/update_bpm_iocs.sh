6cbef51705472ceeee676764e0dac7b28fb52381
Lucas Russo <lerwys@gmail.com>
2019-04-04 14:13:40 -0300

    scripts: add simples scripts for updating HALCS/BPM/TIM

---
+++ scripts/update_bpm_iocs.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+CRATES=()
+CRATES+=("IA-01RaBPM-CO-IOCSrv")
+CRATES+=("IA-02RaBPM-CO-IOCSrv")
+CRATES+=("IA-03RaBPM-CO-IOCSrv")
+CRATES+=("IA-04RaBPM-CO-IOCSrv")
+CRATES+=("IA-05RaBPM-CO-IOCSrv")
+CRATES+=("IA-06RaBPM-CO-IOCSrv")
+CRATES+=("IA-07RaBPM-CO-IOCSrv")
+CRATES+=("IA-08RaBPM-CO-IOCSrv")
+CRATES+=("IA-09RaBPM-CO-IOCSrv")
+CRATES+=("IA-10RaBPM-CO-IOCSrv")
+CRATES+=("IA-11RaBPM-CO-IOCSrv")
+CRATES+=("IA-12RaBPM-CO-IOCSrv")
+CRATES+=("IA-13RaBPM-CO-IOCSrv")
+CRATES+=("IA-14RaBPM-CO-IOCSrv")
+CRATES+=("IA-15RaBPM-CO-IOCSrv")
+CRATES+=("IA-16RaBPM-CO-IOCSrv")
+CRATES+=("IA-17RaBPM-CO-IOCSrv")
+CRATES+=("IA-18RaBPM-CO-IOCSrv")
+CRATES+=("IA-19RaBPM-CO-IOCSrv")
+CRATES+=("IA-20RaBPM-CO-IOCSrv")
+CRATES+=("IA-20RaBPMTL-CO-IOCSrv")
+
+for crate in "${CRATES[@]}"; do
+
+    SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
+	    set -x && \
+	    cd /root/postinstall/apps/bpm-app/bpm-epics-ioc && \
+	    git fetch --all && \
+        git checkout -b stable-\$(date +%Y%m%d-%H%M%S) && \
+	    git checkout master && \
+	    git reset --hard origin/master && \
+	    cp /etc/sysconfig/bpm-epics-ioc /home/lnls-bpm/bpm-epics-ioc.temp && \
+	    systemctl stop halcs-be-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24} && \
+	    make clean uninstall && \
+	    make && \
+	    make install && \
+	    mv /home/lnls-bpm/bpm-epics-ioc.temp /etc/sysconfig/bpm-epics-ioc && \
+	    systemctl daemon-reload && \
+	    systemctl start halcs-be-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}"
+
+done
