ed8a520d5d2de63d3bcaf73d168710502058feba
Lucas Russo <lerwys@gmail.com>
2019-06-26 16:32:12 -0300

    scripts: add script to shutdown SR/TS BPMs IOCs

---
+++ scripts/shutdown_bpm_iocs.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+CRATES=()
+CRATES+=("IA-01RaBPM-CO-IOCSrv")
+CRATES+=("IA-02RaBPM-CO-IOCSrv")
+CRATES+=("IA-03RaBPM-CO-IOCSrv")
+CRATES+=("IA-04RaBPM-CO-IOCSrv")
+CRATES+=("IA-05RaBPM-CO-IOCSrv")
+CRATES+=("IA-06RaBPM-CO-IOCSrv")
+CRATES+=("IA-07RaBPM-CO-IOCSrv")
+CRATES+=("IA-08RaBPM-CO-IOCSrv")
+CRATES+=("IA-09RaBPM-CO-IOCSrv")
+CRATES+=("IA-10RaBPM-CO-IOCSrv")
+CRATES+=("IA-11RaBPM-CO-IOCSrv")
+CRATES+=("IA-12RaBPM-CO-IOCSrv")
+CRATES+=("IA-13RaBPM-CO-IOCSrv")
+CRATES+=("IA-14RaBPM-CO-IOCSrv")
+CRATES+=("IA-15RaBPM-CO-IOCSrv")
+CRATES+=("IA-16RaBPM-CO-IOCSrv")
+CRATES+=("IA-17RaBPM-CO-IOCSrv")
+CRATES+=("IA-18RaBPM-CO-IOCSrv")
+CRATES+=("IA-19RaBPM-CO-IOCSrv")
+CRATES+=("IA-20RaBPM-CO-IOCSrv")
+
+for crate in "${CRATES[@]}"; do
+
+    SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
+        set -x && \
+        systemctl stop halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20}.target" &
+
+done
+
+CRATES=()
+CRATES+=("IA-20RaBPMTL-CO-IOCSrv")
+
+for crate in "${CRATES[@]}"; do
+
+    SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
+        set -x && \
+        systemctl stop halcs-ioc@{17,18,19,20,21}.target" &
+
+done
