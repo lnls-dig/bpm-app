ce664b56a48f398a5eb4dba555e9332ee22c9633
Lucas Russo <lerwys@gmail.com>
2019-07-25 11:00:39 -0300

    scripts: fix zabbix-agent package name typo

--- scripts/install_zabbix_agent.sh
+++ scripts/install_zabbix_agent.sh
@@ -28,7 +28,7 @@ for crate in "${CRATES[@]}"; do
     SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
         set -x && \
         sudo rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm && \
-        sudo yum install -y zabbix_agent || /bin/true && \
+        sudo yum install -y zabbix-agent || /bin/true && \
         sudo systemctl enable zabbix-agent && \
         cat <<'EOF' > /etc/zabbix/zabbix_agentd.conf
 # This is a configuration file for Zabbix agent daemon (Unix)
