3462773a80f189022e311d5ede693645c549fae8
Lucas Russo <lerwys@gmail.com>
2020-06-30 09:22:37 -0300

    scripts: add override script for malamute environment unit

---
+++ scripts/add_override_conf_malamute_log_system.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+CRATES=()
+CRATES+=("IA-01RaBPM-CO-IOCSrv")
+CRATES+=("IA-02RaBPM-CO-IOCSrv")
+CRATES+=("IA-03RaBPM-CO-IOCSrv")
+CRATES+=("IA-04RaBPM-CO-IOCSrv")
+CRATES+=("IA-05RaBPM-CO-IOCSrv")
+CRATES+=("IA-06RaBPM-CO-IOCSrv")
+CRATES+=("IA-07RaBPM-CO-IOCSrv")
+CRATES+=("IA-08RaBPM-CO-IOCSrv")
+CRATES+=("IA-09RaBPM-CO-IOCSrv")
+CRATES+=("IA-10RaBPM-CO-IOCSrv")
+CRATES+=("IA-11RaBPM-CO-IOCSrv")
+CRATES+=("IA-12RaBPM-CO-IOCSrv")
+CRATES+=("IA-13RaBPM-CO-IOCSrv")
+CRATES+=("IA-14RaBPM-CO-IOCSrv")
+CRATES+=("IA-15RaBPM-CO-IOCSrv")
+CRATES+=("IA-16RaBPM-CO-IOCSrv")
+CRATES+=("IA-17RaBPM-CO-IOCSrv")
+CRATES+=("IA-18RaBPM-CO-IOCSrv")
+CRATES+=("IA-19RaBPM-CO-IOCSrv")
+CRATES+=("IA-20RaBPM-CO-IOCSrv")
+CRATES+=("IA-20RaBPMTL-CO-IOCSrv")
+
+for crate in "${CRATES[@]}"; do
+
+    SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
+        set -x && \
+	mkdir -p /etc/systemd/system/malamute.service.d && \
+	cat << EOF > /etc/systemd/system/malamute.service.d/override.conf 
+[Service]
+Environment=\"ZSYS_LOGSYSTEM=false\"
+EOF
+" &
+
+done
