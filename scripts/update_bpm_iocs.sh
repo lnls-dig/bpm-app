c1bf941a4c11f943c3f2f4b53770080659c02598
guilherme.ricioli <guilherme.riciolic@gmail.com>
2022-04-28 16:16:46 -0300

    Remove unused halcs-ioc instances

--- scripts/update_bpm_iocs.sh
+++ scripts/update_bpm_iocs.sh
@@ -42,7 +42,7 @@ for crate in "${CRATES[@]}"; do
         git reset --hard ${COMMIT_ID} && \
         git checkout -b stable-\$(date +%Y%m%d-%H%M%S) && \
         cp /etc/sysconfig/bpm-epics-ioc /home/lnls-bpm/bpm-epics-ioc.temp && \
-        systemctl stop halcs-ioc@{5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
+        systemctl stop halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
         make clean && \
         make && \
         make install && \
@@ -50,6 +50,6 @@ for crate in "${CRATES[@]}"; do
         mv /home/lnls-bpm/bpm-epics-ioc.temp /etc/sysconfig/bpm-epics-ioc && \
         chown -R bpm-epics-ioc:bpm-epics-ioc /opt/epics/ioc/bpm-epics-ioc && \
         systemctl daemon-reload && \
-        systemctl start halcs-ioc@{5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target" &
+        systemctl start halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target" &
 
 done
