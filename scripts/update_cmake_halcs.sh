bd4d1a520f88bff05c95cfc32379d2e8bf491f7a
guilherme.ricioli <guilherme.riciolic@gmail.com>
2022-04-28 17:49:04 -0300

    Start fofb-ioc after halcs update
    Also remove unused halcs-ioc instances.

--- scripts/update_cmake_halcs.sh
+++ scripts/update_cmake_halcs.sh
@@ -62,7 +62,8 @@ for crate in "${CRATES[@]}"; do
         ldconfig && \
         mv /home/lnls-bpm/halcs.cfg.temp /etc/halcs/halcs.cfg && \
         systemctl daemon-reload && \
-        systemctl start halcs-ioc@{5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
+        systemctl start fofb-ioc@5 && \
+        systemctl start halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
         systemctl start tim-rx-ioc@1" &
 
 done
