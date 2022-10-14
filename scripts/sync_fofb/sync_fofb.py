bb8ea5bcc01e5cf7be01751aec987093e44ec93b
Érico Nogueira <erico.rolim@lnls.br>
2022-10-14 11:40:47 -0300

    Speed up sync_fofb.py with wait_for_connection.
    This is following Ana's (@anacso17) recommendations for using
    wait_for_connection() for all PVs (though we skip the single EVG one
    that's the last step of the script).
    
    The consume() function came from [1] as a clean way to loop through all
    members of the function with a list comprehension and without wasting
    memory.
    
    Since we are here, also make the time_frame_len value a variable that's
    set at the top of the script.
    
    Results (reproducible) for runtime (with the sleep(.3) block removed)
    were:
    
    Before this commit:
    
        real    0m55.601s
        user    0m7.838s
        sys     0m0.810s
    
    With this commit:
    
        real    0m27.843s
        user    0m4.902s
        sys     0m0.676s
    
    [1] https://stackoverflow.com/questions/50937966/fastest-most-pythonic-way-to-consume-an-iterator

--- scripts/sync_fofb/sync_fofb.py
+++ scripts/sync_fofb/sync_fofb.py
@@ -10,6 +10,7 @@ Modified by: Érico Nogueira, Guilherme Ricioli
 
 from time import sleep
 from epics import PV
+import collections
 import numpy as np
 import sys
 
@@ -26,6 +27,17 @@ slots = [rtmlamp_slot, 13, 15, 17, 19]
 
 trigger_chans = [1, 2, 20]
 
+time_frame_len_val = 5000
+
+def consume(iterator):
+	collections.deque(iterator, maxlen=0)
+
+global_pv_list = []
+def create_pv(name):
+	pv = PV(name)
+	global_pv_list.append(pv)
+	return pv
+
 def pv_prefix_gen(slot, crate):
 	pv_prefix = ""
 	if slot == rtmlamp_slot:
@@ -40,15 +52,10 @@ def pv_prefix_gen(slot, crate):
 
 def fofb_ctrl_pv_list_gen(name, slot, crate):
 	pv_prefix = pv_prefix_gen(slot, crate)
-	pv_list = [PV(pv_prefix + "DCCP2P" + name)]
+	pv_list = [create_pv(pv_prefix + "DCCP2P" + name)]
 	if slot == rtmlamp_slot:
 		# has additional FOFB_CC core
-		pv_list.append(PV(pv_prefix + "DCCFMC" + name))
-
-	for pv in pv_list:
-		success = pv.wait_for_connection(5)
-		if not success:
-			raise RuntimeError(f"connection to '{pv.pvname}' wasn't successful")
+		pv_list.append(create_pv(pv_prefix + "DCCFMC" + name))
 
 	return pv_list
 
@@ -64,36 +71,50 @@ def put_pv(pv_list, value):
 			print(f"{pv.info}")
 			raise RuntimeError(f"writing into '{pv.pvname}' wasn't successful: '{new_value}' != '{value}'")
 
+cc_enable = {}
+bpm_id = {}
+time_frame_len = {}
+rcv_src = {}
+rcv_in_sel = {}
+for crate in crates:
+	for slot in slots:
+		key = (crate, slot)
+		cc_enable[key] = fofb_ctrl_pv_list_gen("CCEnable-SP", slot, crate)
+		bpm_id[key] = fofb_ctrl_pv_list_gen("BPMId-SP", slot, crate)
+		time_frame_len[key] = fofb_ctrl_pv_list_gen("TimeFrameLen-SP", slot, crate)
+
+		pv_prefix = pv_prefix_gen(slot, crate)
+		if slot != rtmlamp_slot:
+			rcv_src[key] = []
+			rcv_in_sel[key] = []
+			for trigger in trigger_chans:
+				rcv_src[key].extend([create_pv(pv_prefix + "TRIGGER" + str(trigger) + "RcvSrc-Sel")])
+				rcv_in_sel[key].extend([create_pv(pv_prefix + "TRIGGER" + str(trigger) + "RcvInSel-SP")])
+
+consume((pv.wait_for_connection() for pv in global_pv_list))
+
 for crate in crates:
 	print(f"Configuring crate {crate}...")
 
 	for slot in slots:
-		pv_prefix = pv_prefix_gen(slot, crate)
-		phys_slot = (slot + 1) // 2
+		key = (crate, slot)
 
-		cc_enable = fofb_ctrl_pv_list_gen("CCEnable-SP", slot, crate)
-		bpm_id = fofb_ctrl_pv_list_gen("BPMId-SP", slot, crate)
-		time_frame_len = fofb_ctrl_pv_list_gen("TimeFrameLen-SP", slot, crate)
-
-		put_pv(cc_enable, 0)
-
-		put_pv(time_frame_len, 5000)
+		put_pv(cc_enable[key], 0)
+		put_pv(time_frame_len[key], time_frame_len_val)
 
 		bpm_id_value = None
+		phys_slot = (slot + 1) // 2
 		if slot == rtmlamp_slot:
 			bpm_id_value = fofb_ctrl_offs + int(crate) - 1
 		else:
 			bpm_id_value = 8*(int(crate) - 1) + 2*(phys_slot - 7)
-		put_pv(bpm_id, bpm_id_value)
+		put_pv(bpm_id[key], bpm_id_value)
 
-		put_pv(cc_enable, 1)
+		put_pv(cc_enable[key], 1)
 
 		if slot != rtmlamp_slot:
-			for trigger in trigger_chans:
-				rcv_src = [PV(pv_prefix + "TRIGGER" + str(trigger) + "RcvSrc-Sel")]
-				rcv_in_sel = [PV(pv_prefix + "TRIGGER" + str(trigger) + "RcvInSel-SP")]
-				put_pv(rcv_src, 0)
-				put_pv(rcv_in_sel, 5)
+			put_pv(rcv_src[key], 0)
+			put_pv(rcv_in_sel[key], 5)
 
 evg_evt10 = PV("AS-RaMO:TI-EVG:Evt10ExtTrig-Cmd")
 evg_evt10.put("ON", wait=True)
