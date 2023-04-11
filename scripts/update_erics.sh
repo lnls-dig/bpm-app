34ce42ddcafe3997fbf6bb7c8a61bc5fc65c6bd5
Érico Nogueira <erico.rolim@lnls.br>
2023-04-11 10:41:03 -0300

    update_erics: add script.
    This script updates - and installs - our μHAL [1] based IOC [2], which
    currently implements only the subset of functionality that's necessary
    for the FOFB IOC.
    
    It was necessary to mask the halcs-be@3 service on the crates so it
    didn't conflict with the FOFB IOC, because we were unable to find what
    was launching that halcs-be instance.
    
    [1] https://github.com/lnls-dig/uhal
    [2] https://github.com/lnls-dig/erics

---
+++ scripts/update_erics.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ERICS="$(realpath "$1")"
+
+if [ ! -e "${ERICS}/utcaApp" ]; then
+    echo "Make sure '${ERICS}' is an erics directory" >&2
+    exit 1
+fi
+
+. crate_list.sh
+
+for crate in "${CRATES[@]}"; do
+    echo $crate
+    crate=root@${crate}
+
+    rsync -r "${ERICS}" ${crate}:/opt/
+    ssh ${crate} "
+        mkdir -p /var/opt/erics &&
+        chown bpm-epics-ioc:bpm-epics-ioc /var/opt/erics &&
+        cd /opt/erics &&
+        cp service/95-erics.rules /etc/udev/rules.d &&
+        cp service/erics-fofb@.service /etc/systemd/system &&
+        udevadm control -R &&
+        echo 'epicsEnvSet(TOP, /opt/erics)' >> iocBoot/iocutca/envPaths &&
+        echo 'epicsEnvSet(AUTOSAVE_PATH, /var/opt/erics)' >> iocBoot/iocutca/envPaths &&
+        systemctl daemon-reload &&
+        systemctl --no-block restart erics-fofb@2-1"
+done
