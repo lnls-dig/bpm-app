209bfb88a084044467db65c3c67a043aa8e65235
Érico Nogueira <erico.rolim@lnls.br>
2023-06-13 17:33:06 -0300

    update_bpm_iocs: add uninstall step.
    Uninstall the IOC before updating the repo, otherwise files which are
    removed from the IOC won't be uninstalled from the device. This means we
    need to stop the services earlier.

--- scripts/update_bpm_iocs.sh
+++ scripts/update_bpm_iocs.sh
@@ -8,41 +8,20 @@ fi
 
 COMMIT_ID=$1
 
-CRATES=()
-CRATES+=("IA-01RaBPM-CO-IOCSrv")
-CRATES+=("IA-02RaBPM-CO-IOCSrv")
-CRATES+=("IA-03RaBPM-CO-IOCSrv")
-CRATES+=("IA-04RaBPM-CO-IOCSrv")
-CRATES+=("IA-05RaBPM-CO-IOCSrv")
-CRATES+=("IA-06RaBPM-CO-IOCSrv")
-CRATES+=("IA-07RaBPM-CO-IOCSrv")
-CRATES+=("IA-08RaBPM-CO-IOCSrv")
-CRATES+=("IA-09RaBPM-CO-IOCSrv")
-CRATES+=("IA-10RaBPM-CO-IOCSrv")
-CRATES+=("IA-11RaBPM-CO-IOCSrv")
-CRATES+=("IA-12RaBPM-CO-IOCSrv")
-CRATES+=("IA-13RaBPM-CO-IOCSrv")
-CRATES+=("IA-14RaBPM-CO-IOCSrv")
-CRATES+=("IA-15RaBPM-CO-IOCSrv")
-CRATES+=("IA-16RaBPM-CO-IOCSrv")
-CRATES+=("IA-17RaBPM-CO-IOCSrv")
-CRATES+=("IA-18RaBPM-CO-IOCSrv")
-CRATES+=("IA-19RaBPM-CO-IOCSrv")
-CRATES+=("IA-20RaBPM-CO-IOCSrv")
-CRATES+=("IA-20RaBPMTL-CO-IOCSrv")
+. ./crate_list.sh
 
 for crate in "${CRATES[@]}"; do
-
-    SSHPASS=root sshpass -e ssh -o StrictHostKeyChecking=no root@${crate} bash -c "\
+    ssh root@${crate} "
         set -x && \
         mkdir -p /opt/epics/ioc && \
         cd /opt/epics/ioc && \
         (git clone https://github.com/lnls-dig/bpm-epics-ioc || (cd bpm-epics-ioc && git fetch --all)) && \
         cd /opt/epics/ioc/bpm-epics-ioc && \
+        systemctl stop halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
+        cp /etc/sysconfig/bpm-epics-ioc /home/lnls-bpm/bpm-epics-ioc.temp && \
+        make uninstall && \
         git reset --hard ${COMMIT_ID} && \
         git checkout -b stable-\$(date +%Y%m%d-%H%M%S) && \
-        cp /etc/sysconfig/bpm-epics-ioc /home/lnls-bpm/bpm-epics-ioc.temp && \
-        systemctl stop halcs-ioc@{7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24}.target && \
         make clean && \
         make && \
         make install && \
