2b793237e36a63e8c23536389003afb62050a355
Érico Nogueira <erico.rolim@lnls.br>
2023-07-19 14:31:15 -0300

    update_erics: add timing IOC service.

--- scripts/update_erics.sh
+++ scripts/update_erics.sh
@@ -21,10 +21,10 @@ for crate in "${CRATES[@]}"; do
         chown bpm-epics-ioc:bpm-epics-ioc /var/opt/erics &&
         cd /opt/erics &&
         cp service/95-erics.rules /etc/udev/rules.d &&
-        cp service/erics-fofb@.service /etc/systemd/system &&
+        cp service/erics-{tim,fofb}@.service /etc/systemd/system &&
         udevadm control -R &&
         echo 'epicsEnvSet(TOP, /opt/erics)' >> iocBoot/iocutca/envPaths &&
         echo 'epicsEnvSet(AUTOSAVE_PATH, /var/opt/erics)' >> iocBoot/iocutca/envPaths &&
         systemctl daemon-reload &&
-        systemctl --no-block restart erics-fofb@2-1"
+        systemctl --no-block restart erics-tim@1 erics-fofb@2-1"
 done
