f0e779e82a08d3f17b38a1464e4cab9c9a74836a
Érico Nogueira <erico.rolim@lnls.br>
2023-07-25 14:14:41 -0300

    update_erics: change runtime user.
    Use "iocs", since it's more generic. Also chown recursively so the
    autosave files can be overwritten. We also create the user on the
    machine, as needed.
    
    Since we are here, also add '--exclude .git' to our rsync command. This
    is helpful in avoiding issues when copying from main git repositories
    and from worktrees, and eliminates unnecessary network traffic.

--- scripts/update_erics.sh
+++ scripts/update_erics.sh
@@ -4,6 +4,8 @@ set -eu
 
 ERICS="$(realpath "$1")"
 
+USER=iocs
+
 if [ ! -e "${ERICS}/utcaApp" ]; then
     echo "Make sure '${ERICS}' is an erics directory" >&2
     exit 1
@@ -15,10 +17,10 @@ for crate in "${CRATES[@]}"; do
     echo $crate
     crate=root@${crate}
 
-    rsync -r "${ERICS}" ${crate}:/opt/
+    rsync -r --exclude ".git" "${ERICS}" ${crate}:/opt/
     ssh ${crate} "
-        mkdir -p /var/opt/erics &&
-        chown bpm-epics-ioc:bpm-epics-ioc /var/opt/erics &&
+        mkdir -p /var/opt/erics && (useradd $USER || true) &&
+        chown -R ${USER}:${USER} /var/opt/erics &&
         cd /opt/erics &&
         cp service/95-erics.rules /etc/udev/rules.d &&
         cp service/erics-{tim,fofb}@.service /etc/systemd/system &&
