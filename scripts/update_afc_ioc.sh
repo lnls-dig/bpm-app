0203a414d43895f3656f8edd0a227f414d32204c
Érico Nogueira <erico.rolim@lnls.br>
2023-08-09 12:11:48 -0300

    update_erics: rename to update_afc_ioc.
    This commit also adapts the script to support the changes made in
    afc-epics-ioc re. using a single service name for the IOCs.
    
    To avoid impacting operation heavily, we are still using the old
    autosave directory. Furthermore, in order to perform this update
    properly if erics is still running, the following commands are
    necessary:
    
        rm -f /etc/udev/rules.d/95-erics.rule
        systemctl stop erics-tim@1 erics-fofb@2-1

---
+++ scripts/update_afc_ioc.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eu
+
+AFC_IOC="$(realpath "$1")"
+DECODE_REG="$(realpath "$2")"
+
+USER=iocs
+
+if [ ! -e "${AFC_IOC}/utcaApp" ]; then
+    echo "Make sure '${AFC_IOC}' is an afc-epics-ioc directory" >&2
+    exit 1
+fi
+
+if [ ! -x "${DECODE_REG}" ]; then
+    echo "Make sure '${DECODE_REG}' is a decode-reg executable" >&2
+    exit 1
+fi
+
+. crate_list.sh
+
+for crate in "${CRATES[@]}"; do
+    echo $crate
+    crate=root@${crate}
+
+    rsync -r --exclude ".git" "${AFC_IOC}" ${crate}:/opt/
+    rsync "${DECODE_REG}" ${crate}:/usr/local/bin
+    ssh ${crate} "
+        mkdir -p /var/opt/erics && (useradd $USER || true) &&
+        chown -R ${USER}:${USER} /var/opt/erics &&
+        cd /opt/afc-epics-ioc &&
+        cp service/95-afc.rules /etc/udev/rules.d &&
+        cp service/afc-ioc@.service /etc/systemd/system &&
+        udevadm control -R &&
+        echo 'epicsEnvSet(TOP, /opt/afc-epics-ioc)' >> iocBoot/iocutca/envPaths &&
+        echo 'epicsEnvSet(AUTOSAVE_PATH, /var/opt/erics)' >> iocBoot/iocutca/envPaths &&
+        systemctl daemon-reload &&
+        systemctl --no-block restart afc-ioc@{1,2-1}"
+done
