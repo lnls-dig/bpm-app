43692dcd9b7d9f7f316084613f4dbaa3e7090195
Érico Nogueira <erico.rolim@lnls.br>
2023-09-06 11:58:12 -0300

    update_rffe_iocs: restart running services.
    This depends on afc-epics-ioc being installed on the system in /opt.

--- scripts/update_rffe_iocs.sh
+++ scripts/update_rffe_iocs.sh
@@ -12,8 +12,6 @@ BRANCH=$1
 
 . crate_list.sh
 
-# TODO: restart containers with outdated base image
-
 for crate in "${CRATES[@]}"; do
     ssh root@${crate} "
         echo $crate
@@ -21,5 +19,6 @@ for crate in "${CRATES[@]}"; do
         cd /opt/rffe-epics-ioc && rm -f docker-compose.yml &&
         wget https://raw.githubusercontent.com/lnls-dig/rffe-epics-ioc/${BRANCH}/deploy/docker-compose.yml &&
         mkdir -p /var/opt/rffe-epics-ioc &&
-        docker-compose pull" &
+        docker-compose pull rffe-ioc-1 &&
+        CRATE_NUMBER=\$(/opt/afc-epics-ioc/iocBoot/iocutca/getCrate.sh) docker-compose up -d \$(docker ps --filter name=rffeepicsioc_rffe-ioc --format '{{.Names}}' | sed -e s/rffeepicsioc_// -e s/_1//)" &
 done
