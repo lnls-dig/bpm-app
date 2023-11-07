lnls-dig/bpm-app

commit fa0fcc11402460a4f96350a0e43f511c7c3d2cf9

pvs: check readback floats properly.

scripts/python/bpm_app/pvs.py
@@ -6,6 +6,7 @@ Author: Érico Nogueira
 from epics import PV
 from time import sleep, clock_gettime, CLOCK_MONOTONIC
 import collections
+import math
 
 timeout = 10
 
@@ -59,8 +60,9 @@ def _wait_pv(wait_list):
 				read_pv = pv_pair.rb
 			else:
 				read_pv = pv_pair.sp
+			check_fn = lambda x: math.isclose(x, value, rel_tol=.1) if isinstance(value, float) else lambda x: x == value
 			for i in range(10):
-				if read_pv.get(use_monitor=False) == value:
+				if check_fn(read_pv.get(use_monitor=False)):
 					break
 				sleep(.1)
 			else:

commit 9de569468e28c8339880477c73fba2dcf7b4da6f

pvs: fix _wait_pv readback check.
Instead of setting check_fn to one of the two lambdas, it was being set
to the first lambda, which returned the result of isclose() for float
values and the second lambda otherwise, meaning it was always True for
non-float values.

Fixes: fa0fcc11402460a4f96350a0e43f511c7c3d2cf9

scripts/python/bpm_app/pvs.py
@@ -60,7 +60,7 @@ def _wait_pv(wait_list):
 				read_pv = pv_pair.rb
 			else:
 				read_pv = pv_pair.sp
-			check_fn = lambda x: math.isclose(x, value, rel_tol=precision) if isinstance(value, float) else lambda x: x == value
+			check_fn = (lambda x: math.isclose(x, value, rel_tol=precision)) if isinstance(value, float) else lambda x: x == value
 			for i in range(10):
 				if check_fn(read_pv.get(use_monitor=False)):
 					break