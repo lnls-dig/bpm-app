4f62a2ca4e18b940bb85411c97e6766d17ece1b6
Henrique F. Simoes <henrique.simoes@lnls.br>
2024-08-05 15:40:42 -0300

    update_rffe_iocs: replace docker by podman.
    In the new GNU/Linux installation on the crate CPUs, Podman has been
    preferred over Docker. Update the scripts accordingly, also adapting the
    parsing of automatically generated container names.
    
    Stopping all services with `podman-compose down` is required to restart
    the IOCs with potentially new image deployed, as `podman-compose up`,
    differently from `docker-compose up`, will not remove any existing
    running container before attempting to start the services. Timeout is
    set to 0, as procServ does not properly receive SIGTERM, making the use
    of the effective signal (SIGKILL) quickier.

--- scripts/update_rffe_iocs.sh
+++ scripts/update_rffe_iocs.sh
@@ -19,6 +19,8 @@ for crate in "${CRATES[@]}"; do
         cd /opt/rffe-epics-ioc && rm -f docker-compose.yml &&
         wget https://raw.githubusercontent.com/lnls-dig/rffe-epics-ioc/${BRANCH}/deploy/docker-compose.yml &&
         mkdir -p /var/opt/rffe-epics-ioc &&
-        docker-compose pull rffe-ioc-1 &&
-        CRATE_NUMBER=\$(/opt/afc-epics-ioc/iocBoot/iocutca/getCrate.sh) docker-compose up -d \$(docker ps --filter name=rffeepicsioc_rffe-ioc --format '{{.Names}}' | sed -e s/rffeepicsioc_// -e s/_1//)" &
+        podman-compose pull rffe-ioc-1 &&
+        services=\$(podman ps --filter name=rffe-epics-ioc_rffe-ioc --format '{{.Names}}' | sed -e s/rffe-epics-ioc_// -e s/_1//) &&
+        podman-compose down -t 0 &&
+        CRATE_NUMBER=\$(/opt/afc-epics-ioc/iocBoot/iocutca/getCrate.sh) podman-compose up -d \$services" &
 done
