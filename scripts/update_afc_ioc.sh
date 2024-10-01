49e124a142014b35819b2755ab0e904ad1bdf0b7
Érico Nogueira <erico.rolim@lnls.br>
2024-10-01 17:02:02 -0300

    update_afc_ioc: switch to deploying static tarball.

--- scripts/update_afc_ioc.sh
+++ scripts/update_afc_ioc.sh
@@ -2,35 +2,31 @@
 
 set -eu
 
-AFC_IOC="$(realpath "$1")"
-DECODE_REG="$(realpath "$2")"
-
 USER=iocs
 
-if [ ! -e "${AFC_IOC}/utcaApp" -a "$(basename "$AFC_IOC")" = afc-epics-ioc ]; then
-    echo "Make sure '${AFC_IOC}' is an afc-epics-ioc directory" >&2
+if [ ! $# -eq 1 ]; then
+    echo "Wrong usage! $0 <TARBALL>"
     exit 1
 fi
 
-if [ ! -x "${DECODE_REG}" -a "$(basename "$DECODE_REG")" = decode-reg ]; then
-    echo "Make sure '${DECODE_REG}' is a decode-reg executable" >&2
+TARBALL=$1
+
+if [ ! -f "$TARBALL" ]; then
+    echo "Missing tarball"
     exit 1
 fi
 
 . crate_list.sh
 
 for crate in "${CRATES[@]}"; do
-    echo $crate
-    login=root@${crate}
-
-    (rsync -rz --exclude ".git" "${AFC_IOC}" ${login}:/opt/
-    rsync -z "${DECODE_REG}" ${login}:/usr/local/bin
-    ssh ${login} "
+    (ssh root@${crate} tar xzf - -C /opt < "$TARBALL"
+    ssh root@${crate} "
         mkdir -p /var/opt/afc-epics-ioc &&
         chown -R ${USER} /var/opt/afc-epics-ioc &&
         cd /opt/afc-epics-ioc &&
         cp service/95-afc.rules /etc/udev/rules.d &&
         cp service/afc-ioc@.service /etc/systemd/system &&
+        cp bin/linux-x86_64/decode-reg /usr/local/bin &&
         udevadm control -R &&
         echo 'epicsEnvSet(TOP, /opt/afc-epics-ioc)' >> iocBoot/iocutca/envPaths &&
         echo 'epicsEnvSet(AUTOSAVE_PATH, /var/opt/afc-epics-ioc)' >> iocBoot/iocutca/envPaths &&
